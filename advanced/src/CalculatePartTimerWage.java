930b8139a608af205d0a83651979366555945bfc
masahiro.ikeda <masahiro.ikeda@MyComputer>
2019-04-22 13:19:00 +0900

    フィールド変数はまだ習っていないため修正

--- advanced/src/CalculatePartTimerWage.java
+++ advanced/src/CalculatePartTimerWage.java
@@ -16,74 +16,76 @@ import java.util.List;
  * ・DateやTimeのような日時に関するデータはlong型で扱うと計算が行いやすくなります。
  */
 public class CalculatePartTimerWage {
-    private static final String SEPARATOR = ",";
-    private static final String PATH_PART_TIMERS = "アルバイト従業員一覧ファイル(PartTimers.csv)のパスを設定して下さい。";
-    private static final String PATH_ATTENDANCES = "勤務履歴ファイル(Attendances.csv)のパスを設定して下さい。";
-    private static final long ONE_MINUTE = 1000 * 60;   // ミリ秒で1分
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		// 定数を宣言
+		final String SEPARATOR = ",";
+		final String PATH_PART_TIMERS = "アルバイト従業員一覧ファイル(PartTimers.csv)のパスを設定して下さい。";
+		final String PATH_ATTENDANCES = "勤務履歴ファイル(Attendances.csv)のパスを設定して下さい。";
+		final long ONE_MINUTE = 1000 * 60; // ミリ秒で1分
 
-        // アルバイト従業員一覧を読み込み、partTimersリストに格納します。
-        List<String> partTimers = null;
-        Path pathPartTimers = Paths.get(PATH_PART_TIMERS);
-        try {
-            partTimers = Files.readAllLines(pathPartTimers, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            // Files#readAllLinesは検査例外なので必ずtry～catch構文を使います。
-            System.out.println("[ERROR] アルバイト従業員一覧ファイルの読み込みに失敗しました。");
-            e.printStackTrace();
-        }
+		// アルバイト従業員一覧を読み込み、partTimersリストに格納します。
+		List<String> partTimers = null;
+		Path pathPartTimers = Paths.get(PATH_PART_TIMERS);
+		try {
+			partTimers = Files.readAllLines(pathPartTimers, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			// Files#readAllLinesは検査例外なので必ずtry～catch構文を使います。
+			System.out.println("[ERROR] アルバイト従業員一覧ファイルの読み込みに失敗しました。");
+			e.printStackTrace();
+		}
 
-        // 勤怠履歴を読み込み、attendancesリストに格納します。
-        List<String> attendances = null;
-        Path pathAttendances = Paths.get(PATH_ATTENDANCES);
-        try {
-            attendances = Files.readAllLines(pathAttendances, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            System.out.println("[ERROR] 勤怠履歴ファイルの読み込みに失敗しました。");
-            e.printStackTrace();
-        }
+		// 勤怠履歴を読み込み、attendancesリストに格納します。
+		List<String> attendances = null;
+		Path pathAttendances = Paths.get(PATH_ATTENDANCES);
+		try {
+			attendances = Files.readAllLines(pathAttendances, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			System.out.println("[ERROR] 勤怠履歴ファイルの読み込みに失敗しました。");
+			e.printStackTrace();
+		}
 
-        // ダブルループで従業員ごとのアルバイト代を計算する
-        for (String partTimer : partTimers) {
-            String[] partTimerDetail = partTimer.split(SEPARATOR);
+		// ダブルループで従業員ごとのアルバイト代を計算する
+		for (String partTimer : partTimers) {
+			String[] partTimerDetail = partTimer.split(SEPARATOR);
 
-            // アルバイト従業員のデータを取得
-            String partTimerId = partTimerDetail[0];
-            String partTimerName = partTimerDetail[1];
-            // 今回はBigDecimalを使ってみます。
-            BigDecimal partTimerWage = new BigDecimal(partTimerDetail[2]);
+			// アルバイト従業員のデータを取得
+			String partTimerId = partTimerDetail[0];
+			String partTimerName = partTimerDetail[1];
+			// 今回はBigDecimalを使ってみます。
+			BigDecimal partTimerWage = new BigDecimal(partTimerDetail[2]);
 
-            BigDecimal totalWage = BigDecimal.ZERO;
+			BigDecimal totalWage = BigDecimal.ZERO;
 
-            for (String attendance : attendances) {
-                String[] attendanceDetail = attendance.split(SEPARATOR);
+			for (String attendance : attendances) {
+				String[] attendanceDetail = attendance.split(SEPARATOR);
 
-                // 計算中のアルバイト従業員の勤怠履歴か判定
-                String partTimerIdOfAttendanceRecode = attendanceDetail[1];
-                if (!partTimerId.equals(partTimerIdOfAttendanceRecode)) {
-                    // 違った場合は次の繰り返し処理に移る
-                    continue;
-                }
+				// 計算中のアルバイト従業員の勤怠履歴か判定
+				String partTimerIdOfAttendanceRecode = attendanceDetail[1];
+				if (!partTimerId.equals(partTimerIdOfAttendanceRecode)) {
+					// 違った場合は次の繰り返し処理に移る
+					continue;
+				}
 
-                // 勤務開始時間と退勤時間から分単位の労働時間を計算する
-                Time startTime = Time.valueOf(attendanceDetail[2]);
-                Time finishTime = Time.valueOf(attendanceDetail[3]);
-                long workTimeByMillisecond = finishTime.getTime() - startTime.getTime();
-                long workTimeByMinute = workTimeByMillisecond / ONE_MINUTE;
+				// 勤務開始時間と退勤時間から分単位の労働時間を計算する
+				Time startTime = Time.valueOf(attendanceDetail[2]);
+				Time finishTime = Time.valueOf(attendanceDetail[3]);
+				long workTimeByMillisecond = finishTime.getTime() - startTime.getTime();
+				long workTimeByMinute = workTimeByMillisecond / ONE_MINUTE;
 
-                // 勤務時間を時間と分の両方で算出する
-                BigDecimal workHour = new BigDecimal(workTimeByMinute / 60);
-                BigDecimal workMinute = new BigDecimal(workTimeByMinute % 60).divide(new BigDecimal(60));
+				// 勤務時間を時間と分の両方で算出する
+				BigDecimal workHour = new BigDecimal(workTimeByMinute / 60);
+				BigDecimal workMinute = new BigDecimal(workTimeByMinute % 60).divide(new BigDecimal(60));
 
-                // 時給 × 労働時間で1日分の給与額を計算する
-                BigDecimal dailyWage = workHour.multiply(partTimerWage).add(workMinute.multiply(partTimerWage)).setScale(0, RoundingMode.DOWN);
+				// 時給 × 労働時間で1日分の給与額を計算する
+				BigDecimal dailyWage = workHour.multiply(partTimerWage).add(workMinute.multiply(partTimerWage))
+						.setScale(0, RoundingMode.DOWN);
 
-                // 合計金額に加算
-                totalWage = totalWage.add(dailyWage);
-            }
+				// 合計金額に加算
+				totalWage = totalWage.add(dailyWage);
+			}
 
-            System.out.println(partTimerName + " さんのお給料は " + totalWage + " 円でした。");
-        }
-    }
+			System.out.println(partTimerName + " さんのお給料は " + totalWage + " 円でした。");
+		}
+	}
 }
--- basic/src/AggregateSales.java
+++ basic/src/AggregateSales.java
@@ -10,52 +10,53 @@ import java.util.List;
  * ポイントは、ファイルから1列ずつデータを読み込んでString#splitで分割して処理しやすくすることです。
  */
 public class AggregateSales {
-    private static final String ITEM_ALL = "全商品";
-    private static final String SEPARATOR = ",";
-    private static final String PATH_SALES = "ローカル環境に合わせて売上ファイル(sales.csv)のパスを設定して下さい。";
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		// 定数を宣言
+		final String ITEM_ALL = "全商品";
+		final String SEPARATOR = ",";
+		final String PATH_SALES = "C:\\Users\\masahiro.ikeda\\git\\java-basic-practice\\basic\\datafile\\sales.csv";
 
-        // 集計する商品の指定があるか否かをコマンドライン引数から受け取ります。
-        // 存在しない商品かチェックできる処理を実装できるとより素晴らしいですね。
-        String targetItem = null;
-        if (args.length > 0) {
-            targetItem = args[0];
-        } else {
-            targetItem = ITEM_ALL;
-        }
+		// 集計する商品の指定があるか否かをコマンドライン引数から受け取ります。
+		// 存在しない商品かチェックできる処理を実装できるとより素晴らしいですね。
+		String targetItem = null;
+		if (args.length > 0) {
+			targetItem = args[0];
+		} else {
+			targetItem = ITEM_ALL;
+		}
 
-        // 売上ファイルを読み込み、全データをList<String>型のsalesListに読み込みます。
-        List<String> sales = null;
-        Path path = Paths.get(PATH_SALES);
-        try {
-            sales = Files.readAllLines(path, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            // Files#readAllLinesは検査例外なので必ずtry～catch構文を使います。
-            System.out.println("[ERROR] 売上ファイルの読み込みに失敗しました。");
-            e.printStackTrace();
-        }
+		// 売上ファイルを読み込み、全データをList<String>型のsalesListに読み込みます。
+		List<String> sales = null;
+		Path path = Paths.get(PATH_SALES);
+		try {
+			sales = Files.readAllLines(path, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			// Files#readAllLinesは検査例外なので必ずtry～catch構文を使います。
+			System.out.println("[ERROR] 売上ファイルの読み込みに失敗しました。");
+			e.printStackTrace();
+		}
 
-        int totalSales = 0;
-        for (String sale : sales) {
+		int totalSales = 0;
+		for (String sale : sales) {
 
-            // String#splitを使って読み込んだ1列分のデータを","で分割し、必要な項目を取り出します。
-            String[] saleData = sale.split(SEPARATOR);
-            String itemName = saleData[1];
-            int itemSale = Integer.parseInt(saleData[2]);
+			// String#splitを使って読み込んだ1列分のデータを","で分割し、必要な項目を取り出します。
+			String[] saleData = sale.split(SEPARATOR);
+			String itemName = saleData[1];
+			int itemSale = Integer.parseInt(saleData[2]);
 
-            // 全商品を集計する場合、又は指定した商品だった場合は合計金額に加算します。
-            if ((targetItem.equals(ITEM_ALL) || targetItem.equals(itemName))) {
-                totalSales += itemSale;
-            }
-        }
+			// 全商品を集計する場合、又は指定した商品だった場合は合計金額に加算します。
+			if ((targetItem.equals(ITEM_ALL) || targetItem.equals(itemName))) {
+				totalSales += itemSale;
+			}
+		}
 
-        // 集計した合計金額を出力します。
-        // 売上がないときは、金額表示する必要はありません。
-        if (totalSales > 0) {
-            System.out.println("今月の " + targetItem + " の売り上げ合計は " + totalSales + " 円です。");
-        } else {
-            System.out.println("今月の " + targetItem + " の売り上げはありません。");
-        }
-    }
+		// 集計した合計金額を出力します。
+		// 売上がないときは、金額表示する必要はありません。
+		if (totalSales > 0) {
+			System.out.println("今月の " + targetItem + " の売り上げ合計は " + totalSales + " 円です。");
+		} else {
+			System.out.println("今月の " + targetItem + " の売り上げはありません。");
+		}
+	}
 }
